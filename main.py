da3ecb33082143f9471eb4cd37e13c07b27ba9bb
lixin <lixin@inke.cn>
2018-03-19 19:49:37 +0800

    Commit first time

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+def main():
+    print('abc')
+
+if __name__ == '__main__':
+    main()
---
+++ monitor.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import requests
+
+class HuobiAPI(object):
+    """
+    Huobi API request methods
+    """
+    def __init__(self):
+        pass
+
+    def get_symbols(self):
+        """
+        Get all coin types
+        :return:
+        """
+        pass
+
+if __name__ == '__main__':
+    api = HuobiAPI()
